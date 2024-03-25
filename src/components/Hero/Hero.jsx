c3c90c7d2b10feba2b756c498ae504764e090346
Aman1326 <73335315+Aman1326@users.noreply.github.com>
2024-03-25 22:10:22 +0530

    second commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Hero.css"
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from 'react-countup'
+import {motion, spring} from 'framer-motion'
 
 const Hero = () => {
   return (
@@ -12,11 +13,17 @@ const Hero = () => {
         <div className="flexColStart hero-left">
             <div className="hero-title">
                 <div className="orange-circle"/>
-                <h1>
+                <motion.h1 initial={{y:"2rem", opacity:0}}
+                animate={{y:0, opacity:1}}
+                transition={{
+                    duration:2,
+                    type:spring
+                }}
+                >
                     Discover <br/>
                     Most Suitable <br /> 
                     Property
-                </h1>
+                </motion.h1>
             </div>
             <div className="flexColStart hero-desc secondaryText">
                 <span>Find a variety of properties that suits you very easily</span>
@@ -52,11 +59,18 @@ const Hero = () => {
             </div>
         </div>
         {/* right side */}
-        <div className=" flexCenter hero-right">
+        <motion.div 
+        initial={{x:"7rem", opacity:0}}
+        animate={{x:0,opacity:1}}
+        transition={{
+            duration: 2,
+            type: "spring"
+        }}
+        className=" flexCenter hero-right">
             <div className="img-container">
                 <img src="./hero-image.png" alt="house img" />
             </div>
-        </div>
+        </motion.div>
     </div>
    </section>
   )
